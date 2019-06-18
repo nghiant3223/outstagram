8e479f83f95654d6b6e3b7804cfd493ec4142764
ntncsebku <ntncsebku@gmail.com>
2019-06-18 16:32:17 +0700

    Only use 'AutoMigrate' in development mode

--- server/db/main.go
+++ server/db/main.go
@@ -9,45 +9,48 @@ import (
 	"outstagram/server/models"
 )
 
-var dbInstance *gorm.DB
+var dbConn *gorm.DB
 
 func New() (*gorm.DB, error) {
-	if dbInstance == nil {
+	if dbConn == nil {
 		var err error
 
 		// If on TEST mode
 		if flag.Lookup("test.v") != nil {
-			dbInstance, err = gorm.Open("mysql", "root:root@/outstagram?charset=utf8&parseTime=True&loc=Local")
+			dbConn, err = gorm.Open("mysql", "root:root@/outstagram?charset=utf8&parseTime=True&loc=Local")
 		} else {
-			dbInstance, err = gorm.Open(viper.GetString("db.dialect"), viper.GetString("db.url"))
+			dbConn, err = gorm.Open(viper.GetString("db.dialect"), viper.GetString("db.url"))
 		}
 
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 
-		dbInstance.LogMode(true)
-		dbInstance.Debug()
+		dbConn.SingularTable(true)
 
-		dbInstance.SingularTable(true)
-		dbInstance.AutoMigrate(
-			&models.Comment{},
-			&models.Commentable{},
-			&models.Image{},
-			&models.Message{},
-			&models.Notification{},
-			&models.NotifBoard{},
-			&models.Post{},
-			&models.PostImage{},
-			&models.React{},
-			&models.Reactable{},
-			&models.Reply{},
-			&models.Room{},
-			&models.StoryBoard{},
-			&models.StoryImage{},
-			&models.User{},
-			&models.Viewable{})
+		if viper.GetString("env") != "production" {
+			dbConn.LogMode(true)
+			dbConn.Debug()
+
+			dbConn.AutoMigrate(
+				&models.Comment{},
+				&models.Commentable{},
+				&models.Image{},
+				&models.Message{},
+				&models.Notification{},
+				&models.NotifBoard{},
+				&models.Post{},
+				&models.PostImage{},
+				&models.React{},
+				&models.Reactable{},
+				&models.Reply{},
+				&models.Room{},
+				&models.StoryBoard{},
+				&models.StoryImage{},
+				&models.User{},
+				&models.Viewable{})
+		}
 	}
 
-	return dbInstance, nil
+	return dbConn, nil
 }
