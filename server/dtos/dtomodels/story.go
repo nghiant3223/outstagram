fae8b83302e2a83d31363e0c04641bc011ab0f1f
ntncsebku <ntncsebku@gmail.com>
2019-06-21 09:44:04 +0700

    server/Add Duration to Story model

--- server/dtos/dtomodels/story.go
+++ server/dtos/dtomodels/story.go
@@ -1,13 +1,17 @@
 package dtomodels
 
+import "time"
+
 type Story struct {
-	ID          uint   `json:"id"`
-	Tiny        string `json:"tiny"`
-	Small       string `json:"small"`
-	Medium      string `json:"medium"`
-	Big         string `json:"big"`
-	Huge        string `json:"huge"`
-	Origin      string `json:"origin"`
-	ReactableID uint   `json:"reactableID"`
-	ViewableID  uint   `json:"viewableID"`
+	ID          uint      `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	Duration    uint      `json:"duration"`
+	Tiny        string    `json:"tiny"`
+	Small       string    `json:"small"`
+	Medium      string    `json:"medium"`
+	Big         string    `json:"big"`
+	Huge        string    `json:"huge"`
+	Origin      string    `json:"origin"`
+	ReactableID uint      `json:"reactableID"`
+	ViewableID  uint      `json:"viewableID"`
 }
