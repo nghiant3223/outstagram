66b7fa3e6f9f6c3435db418de48a6ee0eb20e18f
ntncsebku <ntncsebku@gmail.com>
2019-06-23 20:35:51 +0700

    server/Fix: UserStory is not returned in route '/api/me/storyfeed'

--- server/dtos/dtomodels/story_board.go
+++ server/dtos/dtomodels/story_board.go
@@ -1,11 +1,12 @@
 package dtomodels
 
 type StoryBoard struct {
-	UserID      uint    `json:"userID"`
-	Fullname    string  `json:"fullname"`
-	AvatarURL   *string `json:"avatarURL"`
-	IsMy        bool    `json:"isMy"`
-	HasNewStory bool    `json:"hasNewStory"`
-	Stories     []Story `json:"stories"`
-	StoryCount  int     `json:"storyCount"`
+	StoryBoardID uint    `json:"storyBoardID"`
+	UserID       uint    `json:"userID"`
+	Fullname     string  `json:"fullname"`
+	AvatarURL    *string `json:"avatarURL"`
+	IsMy         bool    `json:"isMy"`
+	HasNewStory  bool    `json:"hasNewStory"`
+	Stories      []Story `json:"stories"`
+	StoryCount   int     `json:"storyCount"`
 }
