41121a58bf57327c91a1e4004a80039a851c6e8e
ntncsebku <ntncsebku@gmail.com>
2019-08-05 15:51:56 +0700

    .

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Input({ width, height, value, onChange, placeHolder }) {
+export default function Input({ width, height, value, onChange, placeHolder, accept }) {
     return (
         <div className="ui input" style={{ width, height }}>
-            <input placeholder={placeHolder} onChange={onChange} value={value} />
+            <input placeholder={placeHolder} onChange={onChange} value={value} accept={accept} />
         </div>
     )
 }
